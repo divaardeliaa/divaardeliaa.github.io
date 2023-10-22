4932be04e9129ebad09e3db964fbcd63cfc578c4
divaardeliaa <147696641+divaardeliaa@users.noreply.github.com>
2023-10-22 22:27:26 +0700

    FINAL

--- index.js
+++ index.js
@@ -1,35 +1,39 @@
-        function imgSlider(anything){
-            document.querySelector('.foto-diva').src=anything;
-        }
+function imgSlider(anything){
+    document.querySelector('.foto-diva').src=anything;
+}
 
-            $(window).scroll(function() {
-        if ($(window).scrollTop() >= 50) {
-            $('.navbar').css('background', 'transparent');
-        } else {
-            $('.navbar').css('background', '#121826');
-        } });   
+    $(window).scroll(function() {
+if ($(window).scrollTop() >= 50) {
+    $('.navbar').css('background', 'transparent');
+} else {
+    $('.navbar').css('background', '#121826');
+} });   
 
-        function validateForm() {
+function validateForm() {
+
+    var name = document.getElementById("fname");
+    var mail = document.getElementById("email");
+    var message = document.getElementById("message");
+
+
+    if  (name.value == "") {
+        alert('Name must be filled out');
+        name.focus();
+        return false;
+    }
+
+
+    var email = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!email.test(mail.value)) {
+        alert('Invalid Email Address');
+        mail.focus();
+        return false;
+    
+    }  
+
+    else{
+      alert("Message has already sent! Thank you");
   
-            var name = document.getElementById("fname");
-            var mail = document.getElementById("email");
-            var message = document.getElementById("message");
+}
 
-        
-            if  (name.value == "") {
-                alert('Name must be filled out');
-                name.focus();
-                return false;
-            }
-
-        
-            var email = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-            if (!email.test(mail.value)) {
-                alert('Invalid Email Address');
-                email.focus();
-                return false;
-            
-            }
-        }
-        
-
+}
